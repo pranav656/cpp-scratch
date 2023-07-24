02016393bee0469b63e486be4e9bb396212b2323
Pranav Sailesh Mani <pranav656@gmail.com>
2023-07-24 22:07:55 +0200

    Update time complexity

--- DP/canConstruct.cpp
+++ DP/canConstruct.cpp
@@ -12,13 +12,15 @@ using namespace std;
 // Recursive approach: Search if the
 // prefix occurs in the targe string, if yes slice the target string (remove the prefix)
 // and recurse further. This does not work well when the string is long
-// Time complexity is O(n^m) where n is the word bank length and m is the
+// Time complexity is O(n^m *m) where n is the word bank length and m is the
 // target length. Why? The height of the tree is going to be the size
 // of the string m in the worst case. In case where there is a prefix match
 // of size 1 consequtively. On every level of recursion you would also
 // branch multiple trees if every word in the word bank was a prefix of
 // the subsequent target strings. So, on each level, you would have to 
-// n different recursions on each level, hence o(n^m).A
+// n different recursions on each level, hence o(n^m). The multiplication
+// by m is because we search for the occurence of the word in the target and slice the 
+// target. This operation takes m time, so total complexity is O(n^m * m)
 bool canConstructRec(string target, const vector<string>& wordBank)
 {
     if(target.empty()) return true;
