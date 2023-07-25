263c43f7f3e8d216f892702c418d3d250ad1c93c
Pranav Sailesh Mani <pranav656@gmail.com>
2023-07-25 09:03:28 +0200

    Add memoized solution

--- DP/canConstruct.cpp
+++ DP/canConstruct.cpp
@@ -21,7 +21,7 @@ using namespace std;
 // n different recursions on each level, hence o(n^m). The multiplication
 // by m is because we search for the occurence of the word in the target and slice the 
 // target. This operation takes m time, so total complexity is O(n^m * m). Space 
-// complexity is O(m*m = m^2) which is O(m) for the recursion tree and O(m) for every time
+// complexity is O(m*m) which is O(m) for the recursion tree and O(m) for every time
 // you store a slice of a string in one of the recursion calls.
 bool canConstructRec(string target, const vector<string>& wordBank)
 {
@@ -39,9 +39,53 @@ bool canConstructRec(string target, const vector<string>& wordBank)
     return false;
 }
 
+
+// Below is the memoized implementation
+// It's the same as the recursive implementation,
+// just that the results of the recursion are stored in
+// a memoized object and returned if it already exists, 
+// preventing re-computation. The time complexity is 
+// O(n*m*m), O(n*m) since we have stemmed the recursion
+// with memoization, the multiplication with m is because
+// of the search and slice operation. Space complexity is
+// still O(m*m)
+unordered_map<string, bool> memoization_table;
+
+bool canConstructMem(string target, const vector<string>& wordBank)
+{
+    if(memoization_table.find(target) != memoization_table.end()) 
+    {
+        return memoization_table[target];
+    }
+    if(target.empty())
+    {
+        memoization_table[target] = true;
+        return true;
+    }
+    for(string word : wordBank)
+    {
+       size_t found = target.find(word);
+       if (found != string::npos && (found == 0))
+       {
+          string suffix = target.substr(word.size());
+          if(canConstructMem(suffix, wordBank))
+          {
+              memoization_table[target] = true;
+              return true;
+          }
+       }
+    }
+    memoization_table[target] = false;
+    return false;   
+}
+
+
 int main() {
    cout<<canConstructRec("abcd", {"ab", "abc", "cd", "def", "abcd"});
    cout<<canConstructRec("skateboard", {"bo", "rd", "ate", "t", "ska", "sk", "boar"});
    // This call will take forever in the recursive implementation
-   //cout<<canConstructRec("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", {"e", "ee", "eee", "eeee", "eeeee"});
+   //cout<<canConstructRec("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", {"e", "ee", "eee", "eeee", "eeeee"});
+   cout<<canConstructMem("abcd", {"ab", "abc", "cd", "def", "abcd"});
+   cout<<canConstructMem("skateboard", {"bo", "rd", "ate", "t", "ska", "sk", "boar"});
+   cout<<canConstructMem("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", {"e", "ee", "eee", "eeee", "eeeee"});
 }
