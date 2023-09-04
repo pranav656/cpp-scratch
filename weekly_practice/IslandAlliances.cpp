1bc6cb3ad7b5121365151833cdc449bc68229f26
Pranav Sailesh Mani <pranav656@gmail.com>
2023-09-04 21:48:44 +0200

    Change to pointer based impl

--- weekly_practice/IslandAlliances.cpp
+++ weekly_practice/IslandAlliances.cpp
@@ -10,18 +10,33 @@ Open question: SHould the graph be dynamically updated to reflect approved propo
 using namespace std;
 using ull = unsigned long long;
 
-map<int, set<int>> graph;
+map<int, set<int>*> graph;
 
 int main() {
    ull n, m , q;
    cin>>n>>m>>q;
-
     for(int i = 0; i<m; i++)
     {  
         int u, v;
         cin >> u >> v;
-        graph[u].insert(v);
-        graph[v].insert(u);
+        if(graph[u] == nullptr)
+        {
+            graph[u] = new set<int>();
+            graph[u]->insert(v);
+        }
+        else
+        {
+            graph[u]->insert(v);
+        }
+        if(graph[v] == nullptr)
+        {
+            graph[v] = new set<int>();
+            graph[v]->insert(u);
+        }
+        else
+        {
+            graph[v]->insert(u);
+        }
     }
 
     for(int i = 0; i<q; i++)
@@ -31,14 +46,14 @@ int main() {
         bool distrusting_pair = false;
         if(graph.find(c1) != graph.end())
         {
-            if(graph[c1].find(c2) != graph[c1].end())
+            if(graph[c1]->find(c2) != graph[c1]->end())
             {
                 distrusting_pair = true;
             }
         }
         else if(graph.find(c2) != graph.end())
         {
-            if(graph[c2].find(c1) != graph[c2].end())
+            if(graph[c2]->find(c1) != graph[c2]->end())
             {
                 distrusting_pair = true;
             }
@@ -48,26 +63,40 @@ int main() {
             distrusting_pair = false;
         }
 
-        if(!distrusting_pair)
+       if(!distrusting_pair)
         {
             cout<<"APPROVE"<<endl;
-           // Probable bug: The merging of countries is 
-           // not correct
-            if(graph.find(c1) != graph.end())
-            {
-                for(int edge : graph[c1])
-                {
-                    graph[c2].insert(edge);
+            set<int>* merged_set = new set<int>();
+            //merge still wrong
+           if(graph.find(c1) != graph.end() && graph[c1] != graph[c2])
+           {
+               if(graph[c1] != nullptr)
+               { 
+                    set<int> original_set_c1 = *graph[c1];
+                    for(int i : original_set_c1)
+                    {
+                        merged_set->insert(i);
+                    }
+                    delete graph[c1];
+                    graph[c1] = nullptr;
                 }
-            }
+           }
             
-            if(graph.find(c2) != graph.end())
-            {
-                for(int edge : graph[c2])
-                {
-                    graph[c1].insert(edge);
-                }
-            }
+           if(graph.find(c2) != graph.end())
+           {
+               if(graph[c2] != nullptr)
+               {
+                      set<int> original_set_c2 = *graph[c2];
+                      for(int i : original_set_c2)
+                      {
+                        merged_set->insert(i);
+                      }
+                      delete graph[c2];
+                      graph[c2] = nullptr;           
+               }
+           }
+            graph[c1] = merged_set;
+            graph[c2] = merged_set;
             
         }
         else
@@ -75,6 +104,10 @@ int main() {
             cout<<"REFUSE"<<endl;
         }
     }
-    
+    set<int> *a = graph[1];
+    for(auto i : *a)
+    {
+        cout<<i<<endl;        
+    }
    return 0;
 }
