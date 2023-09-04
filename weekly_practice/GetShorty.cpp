507787fd093944e2ec060aa37deb55aa2767d948
Pranav Sailesh Mani <pranav656@gmail.com>
2023-09-04 17:08:21 +0200

    Update GetShorty.cpp

--- weekly_practice/GetShorty.cpp
+++ weekly_practice/GetShorty.cpp
@@ -1,6 +1,6 @@
 /*
 https://open.kattis.com/contests/cgbkbq/problems/getshorty
-TODO: COMPLETE SOLUTION
+
 */
 
 #include <bits/stdc++.h>
@@ -12,22 +12,25 @@ struct Node
 {
    bool operator<(const Node& that) const
    {
-      return d > that.d;
+      if(that.d + numeric_limits<double>::epsilon() > d) return true;
+
+       return false;
    }
    Node(double _d, int _n) : d(_d), n(_n)
    {}
-   int d, n;
+   double d, n;
 } ;
 
 int main()
 {
     int n = -1, m = -1;
-    while(n != 0 && m != 0)
+    while(1)
     {
         unordered_set<int> visited;
         double min_distance = DBL_MIN;
         unordered_map<int, vector<pair<int, double>> > edges;
         cin>>n>>m;
+        if(n == 0 && m == 0) break;        
         for(int i = 0; i<m; i++)
         {
             int n1, n2;
@@ -35,9 +38,11 @@ int main()
             cin>>n1>>n2>>w;
             //w = 1.0 - w;
             edges[n1].push_back(make_pair(n2, w));
+            edges[n2].push_back(make_pair(n1, w));
         }
+        
         priority_queue<Node> minHeap;
-        minHeap.emplace(Node(1.0, 0));
+        minHeap.emplace(Node(1.0000, 0));
         while(!minHeap.empty())
         {
             Node minDistanceNode = minHeap.top();
@@ -56,31 +61,17 @@ int main()
                 {
                     // add the distance of the node to the visited 
                     // Note: can be confusing as heap as distance first 
-                    
                     double dist = minDistanceNode.d * edge.second;
-                    
-                   /* if(minDistanceNode.d == 0.0)
-                    {
-                        dist = edge.second;
-                    }
-                    else
-                    {
-                        dist = minDistanceNode.d * edge.second;
-                    }*/
                     minHeap.emplace(Node( dist , edge.first));
                     if(edge.first == n - 1)
                     {
                         min_distance = max(min_distance, dist);
-                        //cout<<n-1<<" "<<min_distance<<endl;
                     }
                 }
             }
-
-            if(visited.size() == m - 1)
-            {
-                cout<<n-1<<" "<<min_distance<<endl;
-            }
         }
+        cout<<fixed<<setprecision(4)<<min_distance<<endl;
     }    
+        
     return 0;
 }
