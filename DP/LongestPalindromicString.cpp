775ef0690f3bfd38895f62c303b8b7423baf0e8b
Pranav Sailesh Mani <pranav656@gmail.com>
2023-10-12 19:31:15 +0530

    add some comments

--- DP/LongestPalindromicString.cpp
+++ DP/LongestPalindromicString.cpp
@@ -8,9 +8,27 @@
 // as though it's at the center and expand outwards.
 // This improves time complexity to O(n^2).
 // (TODO) DP approach:
+// Make a DP table of size O (n*n).
+// The table at (i,j) represents if 
+// substring at position (i,j) is a palindrome.
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
+        int n = s.size();
+        // vector containing indices of the final answer
+        array<int, 2> ans_idx = {0, 0};
+        vector<vector<bool>> dp(n*n, false);
+        // This step represents filling of substrings
+        // of length 1
+        for(int i = 0; i<n; i++)
+        {
+            dp[i][i] = true;
+        }
+        // This represents substrings of length 2
+        // starting at position i
+        for(int i = 0; i< n-1; i++)
+        {
+            dp[i][i+1] = true;
+        }
     }
 };
