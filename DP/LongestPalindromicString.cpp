2ba25bbfafb696227142ab4184a16712ecbf4f4c
Pranav Sailesh Mani <pranav656@gmail.com>
2023-10-12 17:07:55 +0530

    Create LongestPalindromicString.cpp

---
+++ DP/LongestPalindromicString.cpp
@@ -0,0 +1,16 @@
+// LC5 - Longest Palindromic subsequence
+// Brute force solution:
+// Check every position in the string to check
+// if it's a palindrome or not. Time complexity 
+// O(N^2.N = N^3) for this solution. 
+// Optimized approach: 
+// Go through each position and check the substrings
+// as though it's at the center and expand outwards.
+// This improves time complexity to O(n^2).
+// (TODO) DP approach:
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
