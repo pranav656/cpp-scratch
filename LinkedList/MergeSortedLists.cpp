8e9c4d9b0652191a0747ce78be19e6cd45814805
Pranav Sailesh Mani <pranav656@gmail.com>
2023-10-25 10:21:01 +0530

    Update MergeSortedLists.cpp

--- LinkedList/MergeSortedLists.cpp
+++ LinkedList/MergeSortedLists.cpp
@@ -67,3 +67,40 @@ public:
         return res->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// LC21 - Merge two sorted lists
+// elements into the newly created
+// linked list.
+// Recursive approach. Time complexity
+// O(m+n), space complexity (O(m+n)) 
+// owing to the recursive stack
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // if end of wither of list is
+        // reached, return the other one
+        if(!list1) return list2;
+        if(!list2) return list1;
+        if(list1->val < list2->val)
+        {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else
+        {
+            list2->next=mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
