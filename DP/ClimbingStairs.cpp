cd5a987ec6f0d9c94b8991632bb2717bbaffa4f4
Pranav Sailesh Mani <pranav656@gmail.com>
2023-10-28 17:57:53 +0530

    Create ClimbingStairs.cpp

---
+++ DP/ClimbingStairs.cpp
@@ -0,0 +1,19 @@
+// LC70 (Easy) - Climbing Stairs
+// number of ways to reach 1 - 1
+// number of ways to reach 2 - 2
+// other cases can be extended as
+// dp[n] = dp[n-1]+dp[n-2]
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<n+1;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
