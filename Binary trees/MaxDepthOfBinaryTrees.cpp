3c50d4b4d4d61b6f82b9ee989c350d20bf222fda
Pranav Sailesh Mani <pranav656@gmail.com>
2023-10-28 14:16:27 +0530

    Update MaxDepthOfBinaryTrees.cpp

--- Binary trees/MaxDepthOfBinaryTrees.cpp
+++ Binary trees/MaxDepthOfBinaryTrees.cpp
@@ -10,3 +10,38 @@ public:
         return 1+max(HeightLeft, HeightRight);
     }
 };
+
+// LC104 - Max depth of binary tree
+// BFS solution : Time complexity - O(n), 
+// Space complexity O(m) where m is the number 
+// of nodes at each level.
+// Idea is to check the number of levels that exist
+// Push the child nodes into the queue at each level and loop
+// until the queue isemtpy
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        while(!q.empty())
+        {
+            depth++;
+            int len = q.size();
+            for(int i = 0; i<len;i++)
+            {
+                TreeNode *curr = q.front();
+                q.pop();
+                if(curr->left != nullptr) {
+                    q.push(curr->left);
+                }
+                if(curr->right != nullptr) {
+                    q.push(curr->right);
+                }
+            }
+        }
+
+        return depth;
+    }
+};
