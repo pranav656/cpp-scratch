bdbd2e5644a61c569c2b061e649b1d0a1b34e49b
Pranav Sailesh Mani <pranav656@gmail.com>
2023-10-29 15:44:00 +0530

    Update BinaryTreePostOrderTraversal.cpp

--- Binary trees/BinaryTreePostOrderTraversal.cpp
+++ Binary trees/BinaryTreePostOrderTraversal.cpp
@@ -14,3 +14,31 @@ public:
         return res;
     }
 };
+
+// LC145 - Binary Tree Postorder Traversal
+// Iterative solution with two stacks
+class Solution {
+public:
+    vector<int> res;
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(root == nullptr) return res;
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+        while(!st1.empty())
+        {
+            TreeNode *curr = st1.top(); st1.pop();
+            // current node should be
+            // processed last
+            st2.push(curr);
+            if(curr->left) st1.push(curr->left);
+            if(curr->right) st1.push(curr->right);
+        }
+        while(!st2.empty()) {
+            res.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return res;
+    }
+};
