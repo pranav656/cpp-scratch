40bca25581cb7f5a14def3d2ae01a168e1b2c362
Pranav Sailesh Mani <pranav656@gmail.com>
2023-12-01 12:31:06 +0100

    Update 1_b.cpp

--- AoC2023/1_b.cpp
+++ AoC2023/1_b.cpp
@@ -1,8 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
-
 
 int main()
 {
@@ -10,32 +9,26 @@ int main()
     string line;
     vector<int> nums;
     vector<int> found_nums;
-    array<string, 9> str_nums = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    while(cin>>line)
-    {
-        for(int i = 0; i<line.size(); i++)
-        {
-            if(line[i] >= '0' && line[i]<= '9')
-            {
+    array<string, 9> str_nums = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    while (cin >> line) {
+        for (int i = 0; i < line.size(); i++) {
+            if (line[i] >= '0' && line[i] <= '9') {
                 found_nums.push_back(line[i] - '0');
             }
-            for(int j = 0; j<=8; j++)
-            {
-                if(i+str_nums[j].size()<=line.size())
-                {
-                    if(line.substr(i, str_nums[j].size()) == str_nums[j])
-                    {
+            for (int j = 0; j <= 8; j++) {
+                if (i + str_nums[j].size() <= line.size()) {
+                    if (line.substr(i, str_nums[j].size()) == str_nums[j]) {
                         //cout<<str_nums[j]<<endl;
-                        found_nums.push_back(j+1);
+                        found_nums.push_back(j + 1);
                     }
                 }
             }
-            
         }
-        int num = (found_nums[0])*10 + (found_nums[found_nums.size()-1]);
+        int num = (found_nums[0]) * 10 + (found_nums[found_nums.size() - 1]);
         found_nums.clear();
         //cout<<num<<endl;
         sum += num;
     }
-    cout<<"sum:"<<sum<<endl;
+    cout << "sum:" << sum << endl;
+    return 0;
 }
