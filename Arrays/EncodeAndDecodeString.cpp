ce3406370a4117d74d0b1c6fab79e9e222732e40
Pranav Sailesh Mani <pranav656@gmail.com>
2024-01-16 22:38:29 +0100

    Update EncodeAndDecodeString.cpp

--- Arrays/EncodeAndDecodeString.cpp
+++ Arrays/EncodeAndDecodeString.cpp
@@ -1,3 +1,6 @@
+// LC271 - Encode and decode a vector of strings
+// Straightforward, store the end positions in a vector and
+// use it for decoding. 
 class Solution {
 public:
     vector<int> positions;
@@ -7,26 +10,22 @@ public:
         int curr_pos = 0;
         string encoded_string;
         positions.push_back(curr_pos);
-        cout<<curr_pos<<endl;
         for(auto str:strs)
         {
             encoded_string += str;
             curr_pos += str.size();
             positions.push_back(curr_pos);
-            cout<<curr_pos<<endl;
         }
         return encoded_string;
     }
 
     vector<string> decode(string s) {
         vector<string> decoded_vector;
-        cout<<s<<endl;
         for(int i = 0; i<positions.size()-1; i++)
         {
             int start = positions[i];
             int end = positions[i+1];
             string decoded_string = s.substr(start, end-start);
-            cout<<decoded_string;
             decoded_vector.push_back(decoded_string);
         }
         return decoded_vector;
