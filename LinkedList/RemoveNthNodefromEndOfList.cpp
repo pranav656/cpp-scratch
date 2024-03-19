b5e122b4a9ec83cdd0909c71748f88513936ff74
Pranav Sailesh Mani <pranav656@gmail.com>
2024-03-19 20:46:00 +0100

    Update RemoveNthNodefromEndOfList.cpp

--- LinkedList/RemoveNthNodefromEndOfList.cpp
+++ LinkedList/RemoveNthNodefromEndOfList.cpp
@@ -44,3 +44,36 @@ public:
         return head;
     }
 };
+
+
+// LC19 
+// Remove nth node from list:
+// Single pass solution
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head->next == nullptr) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(n>0)
+        {
+            fast = fast->next;
+            n--;
+        }
+
+        // if the fast pointer has already reached the
+        // end of the list then the first element
+        // has to be removed, so return head->next
+        if(fast == nullptr)
+        {
+            return head->next;
+        }
+        while(fast->next != nullptr)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};
