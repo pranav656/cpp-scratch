ab8f5eb6c80692117f4f867780fd888ec6a66081
Pranav Sailesh Mani <pranav656@gmail.com>
2024-04-13 14:54:34 +0200

    Update FindTheDuplicateNumber.cpp

--- LinkedList/FindTheDuplicateNumber.cpp
+++ LinkedList/FindTheDuplicateNumber.cpp
@@ -39,3 +39,41 @@ public:
         return l; 
     }
 };
+
+// Approach 2 - Linked list
+// One idea is to look at this array as a linked list
+// The index represents the node and the value at the index 
+// represents the next node. We need to know the Floyd's cycle
+// detection algorithm to solve this problem. 
+// First find the intersection point of fast and slow 
+// pointers. Once that is known, you can find the
+// start of the cycle by traversing from the start of the list
+// and the intersection point to find the cycle start (which is the
+// duplicate number in this case). TODO: Revise
+// and think more about this algorithm. 
+// Neetcode video explanation: https://www.youtube.com/watch?v=DfljaUwZsOk
+// Time complexity : O(n)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        // Find the intersection point
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        // start slow2 from the start
+        // of the list
+        int slow2 = 0;
+        while(slow2 != fast)
+        {
+            slow2 = nums[slow2];
+            fast = nums[fast];
+        }
+        // the intersection point is
+        // the start of the cycle, i.e. the duplicate.
+        return slow2;
+    }
+};
