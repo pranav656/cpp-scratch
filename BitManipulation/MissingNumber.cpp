d43e5c0b0d18527b78034fa659e3089cc7bd098e
Pranav Sailesh Mani <pranav656@gmail.com>
2024-04-27 21:55:15 +0200

    Create MissingNumber.cpp

---
+++ BitManipulation/MissingNumber.cpp
@@ -0,0 +1,20 @@
+// LC268 - Missing number
+//O(n) memory complexity solution is easy
+// The trick to get O(1) solution is to use
+// XOR. You XOR the input array with the ideal
+// array where all elements are present. The
+// missing number is left out and that's the
+// answer.
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = nums.size(); // not a part of loop
+        int n = nums.size();
+        for(int i = 0; i<n; i++)
+        {
+            // xoring
+            ans ^= nums[i] ^ i;
+        }
+        return ans;
+    }
+};
