dcecf1b8c6a716ebd9f9924aaa133e960fb60826
Pranav Sailesh Mani <pranav656@gmail.com>
2024-05-15 06:23:23 +0200

    Create DesignHashset.cpp

---
+++ Hashmap/DesignHashset.cpp
@@ -0,0 +1,29 @@
+// LC705 - Design Hashset
+class MyHashSet {
+public:
+    vector<int> v;
+    MyHashSet() {
+        // 10^6
+        v.resize(10000001+1);
+    }
+    
+    void add(int key) {
+        v[key] = 1;
+    }
+    
+    void remove(int key) {
+        v[key] = 0;
+    }
+    
+    bool contains(int key) {
+        return (v[key]==1);
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
