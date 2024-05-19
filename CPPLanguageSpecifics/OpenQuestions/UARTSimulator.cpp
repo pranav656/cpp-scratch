4a6360dc1bb58c27d45c213f0beb3f6508678d14
Pranav Sailesh Mani <pranav656@gmail.com>
2024-05-19 21:32:34 +0200

    Create UARTSimulator.cpp

---
+++ CPPLanguageSpecifics/OpenQuestions/UARTSimulator.cpp
@@ -0,0 +1,131 @@
+// TODO: Not complete yet
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+class UARTSimulator {
+public:
+    UARTSimulator (const int baudRate) : baudRate(baudRate)
+    {
+        // Transmission time 
+        // simple simulation:
+        // 10 bits are transmitted at a time (8 char bits + 1 start + 1 stop)
+        transmissionTime = (10 * 1000000) / baudRate;
+    }
+
+    void transmit(char data) {
+        std::unique_lock<std::mutex> lock(txMutex);
+        txQueue.push(data);
+        // Notify the transmitter thread
+        // that data is available
+        txCondition.notify_one();
+    }
+
+    void receive()
+    {
+        std::unique_lock<std::mutex> lock(rxMutex);
+        // the statement below is tricky
+        // it releases the mutex and waits for the 
+        // rx queue to be non empty. Once the
+        // queue is non empty, the lock is acquired again
+        rxCondition.wait(lock, [this]{
+            return !rxQueue.empty();
+        });
+
+        char data = rxQueue.front();
+        rxQueue.pop();
+        lock.unlock(); // explicitly release the lock
+
+        std::cout<< "Received data : " << data << std::endl;
+    }
+
+    void start() {
+        // starts the receiver and transmitter asynchronously
+        txThread = std::thread(&UARTSimulator::transmitter, this);
+        rxThread = std::thread(&UARTSimulator::receiver, this);
+    }
+
+    void join() {
+        // synchronization, ensures
+        // both threads are complete
+        // before the program terminates
+        txThread.join();
+        rxThread.join();
+    }
+
+private:
+    int baudRate;
+    int transmissionTime; // in microseconds
+
+    // queue data structure used
+    // to simulate receiving/tranmsitting
+    std::queue<char> txQueue;
+    std::queue<char> rxQueue;
+
+    // mutex to restrict 
+    // access to queueus
+    std::mutex txMutex;
+    std::mutex rxMutex;
+
+    // condition variables are used for
+    // synchronization between threads to manage
+    // transmission and reception of data
+    // Condition variables are used to block
+    // one ore more threads until a particular condition
+    // is met. 
+    std::condition_variable txCondition;
+    std::condition_variable rxCondition;
+
+    // Threads to transmit and
+    // receive data asynchornously
+    std::thread txThread;
+    std::thread rxThread;
+
+    void transmitter() {
+        while(true) {
+            std::unique_lock<std::mutex> lock(txMutex);
+            txCondition.wait(lock, [this] {
+                return !txQueue.empty();
+            });
+
+            char data = txQueue.front();
+            txQueue.pop();
+            lock.unlock();
+
+            // Simulate the transmission time for the whole frame (start bit
+            // data bits and stop bit)
+            std::this_thread::sleep_for(std::chrono::microseconds(transmissionTime));
+            {
+                std::lock_guard<std::mutex> lock(rxMutex);
+                rxQueue.push(data);
+            }
+            rxCondition.notify_one();
+        }
+    }
+
+    void receiver() {
+        while(true) {
+            receive();
+        }
+    }
+};
+
+int main()
+{
+
+    UARTSimulator uart(9600);
+
+    uart.start();
+    uart.transmit('H');
+    uart.transmit('e');
+    uart.transmit('l');
+    uart.transmit('l');
+    uart.transmit('o');
+
+    uart.join();
+    
+    return 0;
+}
